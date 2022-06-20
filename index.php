62038383ed041bf1ff79291e7b19206596bb003b
gyeongsik_lee <elvin2@naver.com>
2022-06-20 16:01:27 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<h1>Hello, <?= $_GET["url"] ?></h1>
+<?php
+/*
+    require_once 'application/libs/Config.php';
+    require_once 'application/libs/Autoload.php';
+    new application\libs\Application();
+    */
