84aac0b644cf0a4540cac24a45c6dc8614511ca4
gyeongsik_lee <elvin2@naver.com>
2022-06-20 18:08:46 +0900

    6/20

---
+++ application/controllers/BoardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace application\controllers;
+
+use application\models\BoardModel;
+
+class BoardController extends Controller
+{
+    public function list()
+    {
+        $model = new BoardModel();
+        // $list = $model->selBoardList();
+        $this->addAttribute("list", $model->selBoardList());
+
+        return "board/list.php"; // view 파일명
+    }
+}
--- index.php
+++ index.php
@@ -1,7 +1,4 @@
-<h1>Hello, <?= $_GET["url"] ?></h1>
 <?php
-/*
-    require_once 'application/libs/Config.php';
-    require_once 'application/libs/Autoload.php';
-    new application\libs\Application();
-    */
+require_once 'application/libs/Config.php';
+require_once 'application/libs/Autoload.php';
+new application\libs\Application();
